2eb5e5dfcf8f75760720fd55cf9a4ace08a288b3
James Badger <james@jamesbadger.ca>
2019-05-31 18:12:46 -0600

    Disable proxies for HTTPS apt repos
    apt-cacher-ng fails on these repos, so DIRECT should be used.

--- sarcee/attributes/default.rb
+++ sarcee/attributes/default.rb
@@ -2,6 +2,6 @@
 default['apt']['http_proxies']  = [];
 default['apt']['http_direct']   = [];
 default['apt']['https_proxies'] = [];
-default['apt']['https_direct']  = ["download.docker.com"];
+default['apt']['https_direct']  = ["download.docker.com", "packagecloud.io"];
 default['apt']['ftp_proxies']   = [];
 default['apt']['ftp_direct']    = [];
