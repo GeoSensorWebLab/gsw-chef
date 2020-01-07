8d6c6ab6da8763b09c082345f418e643b9803af0
James Badger <james@jamesbadger.ca>
2020-01-07 14:06:23 -0700

    Bump gsw_airflow cookbook to v1.0.0

--- gsw_airflow/CHANGELOG.md
+++ gsw_airflow/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog for airflow
 
+## v1.0.0
+
+* Upgrade to Ruby 2.7.0
+* Unlock Chef/test-kitchen to use latest versions
 
 ## v0.1.0
 
+* Initial cookbook created
--- gsw_airflow/metadata.rb
+++ gsw_airflow/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures airflow'
 long_description 'Installs/Configures airflow'
 # issues_url 'https://github.com/<insert_org_here>/gsw-chef/issues' if respond_to?(:issues_url)
 # source_url 'https://github.com/<insert_org_here>/gsw-chef' if respond_to?(:source_url)
-version          '0.1.0'
+version          '1.0.0'
 privacy          true
 
 # Public Cookbooks
