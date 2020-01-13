c8d3061a252eb7c872423139a8a43fc39909b599
James Badger <james@jamesbadger.ca>
2020-01-13 15:23:29 -0700

    Move Host/Service definitions to separate files
    This makes it easier to organize the definitions.

---
+++ crowchild/attributes/hosts.rb
@@ -0,0 +1,68 @@
+# Host Definitions for Icinga2
+# 
+# Sample Host:
+#   name: string, mandatory
+#   address: string, optional
+#   address6: string, optional
+#   groups: array of strings, optional
+#   check_command: string, mandatory
+#
+# I recommend keeping this array sorted by names.
+default['icinga2']['host_objects'] = [
+{
+  name: "airport",
+  address: "10.1.14.229",
+  address6: "2605:fd00:4:1000:f816:3eff:fe37:c7b3",
+  groups: ["geocens"],
+  check_command: "hostalive"
+},
+{
+  name: "barlow",
+  address: "162.246.156.221",
+  address6: "2605:fd00:4:1000:f816:3eff:fe7c:7c2a",
+  groups: ["arcticconnect"],
+  check_command: "hostalive"
+},
+{
+  name: "blackfoot",
+  address: "10.1.3.111",
+  address6: "2605:fd00:4:1000:f816:3eff:fec5:a91d",
+  groups: ["arcticconnect"],
+  check_command: "hostalive"
+},
+{
+  name: "deerfoot",
+  address: "10.1.6.106",
+  address6: "2605:fd00:4:1000:f816:3eff:fe63:b394",
+  groups: ["arcticconnect"],
+  check_command: "hostalive"
+},
+{
+  name: "macleod",
+  address: "199.116.235.109",
+  address6: "2605:fd00:4:1000:f816:3eff:fe83:4b52",
+  groups: ["geocens"],
+  check_command: "hostalive"
+},
+{
+  name: "sarcee",
+  address: "10.1.11.237",
+  address6: "2605:fd00:4:1000:f816:3eff:fee6:b246",
+  groups: ["arcticconnect"],
+  check_command: "hostalive"
+},
+{
+  name: "shaganappi",
+  address: "10.1.0.158",
+  address6: "2605:fd00:4:1000:f816:3eff:fe9e:b42f",
+  groups: ["geocens"],
+  check_command: "hostalive"
+},
+{
+  name: "stoney",
+  address: "199.116.235.80",
+  address6: "2605:fd00:4:1000:f816:3eff:fe17:ad18",
+  groups: ["geocens"],
+  check_command: "hostalive"
+}
+]
