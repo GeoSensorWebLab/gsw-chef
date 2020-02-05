3c8a426788903c71b7c0f4b545fce977aedab795
James Badger <james@jamesbadger.ca>
2020-02-05 16:11:52 -0700

    Remvoe GDAL from Tomcat Environment
    GDAL is not being used for GeoServer.

--- edmonton/recipes/geoserver.rb
+++ edmonton/recipes/geoserver.rb
@@ -108,7 +108,6 @@ systemd_unit "tomcat.service" do
   Environment="CATALINA_HOME=#{tomcat_home}"
   Environment="CATALINA_BASE=#{tomcat_home}"
   Environment="CATALINA_OPTS="
-  Environment="GDAL_DATA=/usr/local/share/gdal"
   Environment="LD_LIBRARY_PATH=$LD_LIBRARY_PATH:#{tomcat_home}/lib"
   Environment="JAVA_OPTS=-Dfile.encoding=UTF-8 -Djava.library.path=/usr/local/lib:#{tomcat_home}/lib -Xms#{node["tomcat"]["Xms"]} -Xmx#{node["tomcat"]["Xmx"]}"
 
