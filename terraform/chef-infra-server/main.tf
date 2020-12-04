52f8471d14dd01cd1d92b0ff4ce2fd37d9a75112
James Badger <james@jamesbadger.ca>
2020-12-03 21:31:42 -0700

    Add Terraform scripts for Chef Infra Server
    Sets up an EC2 instance with related resources to run Chef Infra
    Server. Instance must still be manually configured. Subdomain and
    HTTPS still need to be added.

---
+++ terraform/chef-infra-server/main.tf
@@ -0,0 +1,126 @@
+# Most resources will be in Oregon (US-West-2)
+provider "aws" {
+  allowed_account_ids = ["546905020646"]
+  profile             = "gswlab"
+  region              = "us-west-2"
+  version             = "~> 2.0"
+}
+
+# Some resources must be in the "global" region
+provider "aws" {
+  alias               = "awseast"
+  allowed_account_ids = ["546905020646"]
+  profile             = "gswlab"
+  region              = "us-east-1"
+  version             = "~> 2.0"
+}
+
+########################
+# Key Management Service
+########################
+
+resource "aws_kms_key" "chef_key" {
+  description = "KMS key for Chef Infra Server"
+  
+  tags = {
+    terraform = "chef-infra-server"
+  }
+}
+
+#####
+# IAM
+#####
+
+# This profile will allow other AWS resources to grant access to EC2
+# instances using this profile, instead of having to deploy keys to
+# services running on the instance.
+resource "aws_iam_instance_profile" "chef_server_profile" {
+  name = "chef_server_profile"
+  role = aws_iam_role.chef_role.name
+}
+
+resource "aws_iam_role" "chef_role" {
+  name = "chef_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+
+  tags = {
+    terraform = "chef-infra-server"
+  }
+}
+
+#####
+# EC2
+#####
+
+resource "aws_key_pair" "gswlab_key" {
+  key_name   = "gswlab-aws-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/OgHlCLxG4R99ln/llXYi78O6rnR2KXnhO9U6pqV+LSzAgQQ97VYXoXHQ8RBnsJbfO2V8Ii/SCVJDODXqsaFC1q3VLlZkLzLD9wFYKOsiKGgEq6zzuEbQC2hossClg1j/9oDFuN8DikkWM7gPOysLPo6NwfMkgjm7hLNt2/JTKXoOavXAp6QFi5RPSshK7POcPS3JYKmFTWT9C8mHMS7TMr744J/88/lqims0xsDAoSTJp47KqLi2SXVf7GVttNRrmSpexMvZVtoUVm2Ijv6J0FtR/O6J+qY7IulwxW/WBjcVRTFbEsb4GgPbAWntLMLsFF+8nf20gkj/J9rKT7nj gswlab-aws"
+
+  tags = {
+    terraform = "chef-infra-server"
+  }
+}
+
+data "aws_ami" "ubuntu2004" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "chef_server" {
+  ami                         = data.aws_ami.ubuntu2004.id
+  associate_public_ip_address = true
+  hibernation                 = true
+  iam_instance_profile        = aws_iam_instance_profile.chef_server_profile.name
+  instance_type               = "t2.medium"
+  key_name                    = aws_key_pair.gswlab_key.key_name
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    kms_key_id            = aws_kms_key.chef_key.id
+    volume_size           = 30
+    volume_type           = "gp2"
+  }
+
+  tags = {
+    Name = "ChefInfraServer"
+    terraform = "chef-infra-server"
+  }
+}
+
+output "instance_ip_addr" {
+  value       = aws_instance.chef_server.private_ip
+  description = "The private IP address of the Chef Infra Server instance."
+}
+
+output "instance_pub_ip_addr" {
+  value       = aws_instance.chef_server.public_ip
+  description = "The public IP address of the Chef Infra Server instance."
+}
