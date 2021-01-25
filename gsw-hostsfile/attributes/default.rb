1c6e26ba2d25480318a3a989e63365d4e48042ae
James Badger <james@jamesbadger.ca>
2021-01-25 15:48:55 -0700

    Add beddington RAC node IP
    Update year to 2021
    Remove unused spec/test directories

--- gsw-hostsfile/attributes/default.rb
+++ gsw-hostsfile/attributes/default.rb
@@ -1,17 +1,54 @@
 # Hostsfile entries.
 # These are used for having a shorthand domain that can be used in vhost
-# definitions, and so that changes to IPs can be updated in one 
+# definitions, and so that changes to IPs can be updated in one
 # location.
 default["gsw-hostsfile"]["hostsfile"] = [
-  { "hostname" => "airport.gswlab.ca", "ip" => "10.1.14.229" },
-  { "hostname" => "barlow.gswlab.ca", "ip" => "10.1.11.170" },
-  { "hostname" => "blackfoot.gswlab.ca", "ip" => "10.1.3.111" },
-  { "hostname" => "bow.gswlab.ca", "ip" => "10.1.4.6" },
-  { "hostname" => "cowboy.gswlab.ca", "ip" => "10.1.9.179" },
-  { "hostname" => "crowchild.gswlab.ca", "ip" => "10.1.10.243" },
-  { "hostname" => "deerfoot.gswlab.ca", "ip" => "10.1.6.106" },
-  { "hostname" => "macleod.gswlab.ca", "ip" => "10.1.0.186" },
-  { "hostname" => "sarcee.gswlab.ca", "ip" => "10.1.11.237" },
-  { "hostname" => "shaganappi.gswlab.ca", "ip" => "10.1.0.158" },
-  { "hostname" => "stoney.gswlab.ca", "ip" => "10.1.0.111" }
+  {
+    "hostname" => "airport.gswlab.ca",
+    "ip"       => "10.1.14.229"
+  },
+  {
+    "hostname" => "barlow.gswlab.ca",
+    "ip"       => "10.1.11.170"
+  },
+  {
+    "hostname" => "beddington.gswlab.ca",
+    "ip"       => "10.1.6.97"
+  },
+  {
+    "hostname" => "blackfoot.gswlab.ca",
+    "ip"       => "10.1.3.111"
+  },
+  {
+    "hostname" => "bow.gswlab.ca",
+    "ip"       => "10.1.4.6"
+  },
+  {
+    "hostname" => "cowboy.gswlab.ca",
+    "ip"       => "10.1.9.179"
+  },
+  {
+    "hostname" => "crowchild.gswlab.ca",
+    "ip"       => "10.1.10.243"
+  },
+  {
+    "hostname" => "deerfoot.gswlab.ca",
+    "ip"       => "10.1.6.106"
+  },
+  {
+    "hostname" => "macleod.gswlab.ca",
+    "ip"       => "10.1.0.186"
+  },
+  {
+    "hostname" => "sarcee.gswlab.ca",
+    "ip"       => "10.1.11.237"
+  },
+  {
+    "hostname" => "shaganappi.gswlab.ca",
+    "ip"       => "10.1.0.158"
+  },
+  {
+    "hostname" => "stoney.gswlab.ca",
+    "ip"       => "10.1.0.111"
+  }
 ]
