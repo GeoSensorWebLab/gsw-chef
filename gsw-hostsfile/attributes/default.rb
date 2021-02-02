c9fcb0b8b530f56eef47b93f2dfc704a61fe1d2c
James Badger <james@jamesbadger.ca>
2021-02-01 18:33:26 -0700

    Change IP for beddington instance
    The IP changed after the instance was rebuilt into a m1.tiny size.

--- gsw-hostsfile/attributes/default.rb
+++ gsw-hostsfile/attributes/default.rb
@@ -13,7 +13,7 @@ default["gsw-hostsfile"]["hostsfile"] = [
   },
   {
     "hostname" => "beddington.gswlab.ca",
-    "ip"       => "10.1.6.127"
+    "ip"       => "10.1.2.180"
   },
   {
     "hostname" => "blackfoot.gswlab.ca",
